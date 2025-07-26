7f1a226f87c52a2e84d4af8be4c10fb36abfb83b
Yamin Shihab <yaminshihab123@gmail.com>
2025-07-26 15:34:31 -0600

    feat: randomize header border color with JavaScript

---
+++ main.js
@@ -0,0 +1,20 @@
+const colors = [
+  "#f5e0dc",
+  "#f2cdcd",
+  "#f5c2e7",
+  "#cba6f7",
+  "#f38ba8",
+  "#eba0ac",
+  "#fab387",
+  "#f9e2af",
+  "#a6e3a1",
+  "#94e2d5",
+  "#89dceb",
+  "#74c7ec",
+  "#89b4fa",
+  "#b4befe",
+];
+
+const randomColor = colors[Math.floor(Math.random() * colors.length)];
+document.getElementsByTagName("header")[0].style.borderBottomColor =
+  randomColor;
